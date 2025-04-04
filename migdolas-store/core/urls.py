a41abf7cda63b73e56b9cfda88e5352ebcca0d6a
KarolisT0 <karolistomkus13@gmail.com>
2025-04-04 19:53:20 +0300

    Added product list, and cart. py

--- migdolas-store/core/urls.py
+++ migdolas-store/core/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('parduotuve/', views.product_list, name='product_list'),
+    path('produktas/<slug:slug>/', views.product_detail, name='product_detail')
 ]
--- migdolas-store/core/views.py
+++ migdolas-store/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 def home(request):
@@ -7,3 +7,7 @@ def home(request):
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'product_list.html', {'products': products})
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    return render(request, 'product_detail.html', {'product': product})
